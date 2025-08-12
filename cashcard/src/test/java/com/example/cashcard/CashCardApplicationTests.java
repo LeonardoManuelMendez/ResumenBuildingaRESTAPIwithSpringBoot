0e98a8c5a4ebddc7c2f5394d5328ce5f084f4cf6
Leonardo Méndez <leonardommendez@gmail.com>
2025-08-12 20:55:22 +0200

    8.1 Prueba para enpoint GET

--- cashcard/src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ cashcard/src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.cashcard;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99",
+                String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
